5ab32706d03dcec1da877cfe638c25a1b42f80f2
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-10-15 22:17:32 +0300

    Prettier Applied

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,53 +1,79 @@
 "use client";
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function SingleTickets() {
-    const [formData, setFormData] = useState({
-        email: '',
-        name: '',
-        phone: '',
-        paymentMethod: ''
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    phone: "",
+    paymentMethod: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const response = await fetch("/api/submit-ticket", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    async function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        const response = await fetch('/api/submit-ticket', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-    
-        if (response.ok) {
-            alert((await response.json()).message ?? "An error occurred");
-        } else {
-            console.error('Form submission error');
-        }
+    if (response.ok) {
+      alert((await response.json()).message ?? "An error occurred");
+    } else {
+      console.error("Form submission error");
     }
+  }
 
-    return (
-        <section id="book-one-ticket">
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="email" id="email-input" value={formData.email} onChange={handleChange} />
-                <input type="text" name="name" id="name-input" value={formData.name} onChange={handleChange} />
-                <input type="text" name="phone" id="phone-input" value={formData.phone} onChange={handleChange} />
-                <select name="paymentMethod" id="payment-method" value={formData.paymentMethod} onChange={handleChange}>
-                    <option value="telda">Telda</option>
-                    <option value="instapay">Instapay</option>
-                    <option value="mobile-wallet">Mobile Wallet (VF Cash, etc.)</option>
-                    <option value="school-cash">School Office</option>
-                </select>
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    );
+  return (
+    <section id="book-one-ticket">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          id="email-input"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="name"
+          id="name-input"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="phone"
+          id="phone-input"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+
+        <select
+          name="paymentMethod"
+          id="payment-method"
+          value={formData.paymentMethod}
+          onChange={handleChange}
+        >
+          <option value="telda">Telda</option>
+          <option value="instapay">Instapay</option>
+          <option value="mobile-wallet">Mobile Wallet (VF Cash, etc.)</option>
+          <option value="school-cash">School Office</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <center><h1 className="text-9xl">TEDx!</h1></center>
+      <center>
+        <h1 className="text-9xl">TEDx!</h1>
+      </center>
     </main>
   );
 }
