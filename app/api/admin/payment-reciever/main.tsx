81cd5fa5674cd10338c149ad0d0dee661cb4ca04
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-10-30 17:38:04 +0300

    Critical Bug Fixed

--- app/api/admin/payment-reciever/main.tsx
+++ app/api/admin/payment-reciever/main.tsx
@@ -86,6 +86,7 @@ export async function pay(from: string, amount: string, date: string) {
             { status: 500 }
           );
         }
+        unpaid[i].uuid = randUUID;
         paidFor.push(unpaid[i]);
       } else {
         paid -= unpaid[i].price;
