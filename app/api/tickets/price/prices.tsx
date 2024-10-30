ed265b6f603f52ec22d53478ff86427ec9a78506
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-10-30 15:57:11 +0300

    Bugs and Abstracting away prices

--- app/api/tickets/price/prices.tsx
+++ app/api/tickets/price/prices.tsx
@@ -1,4 +1,6 @@
+import { GROUP_TICKET_PRICE, INDIVIDUAL_TICKET_PRICE } from "@/app/metadata";
+
 export let price = {
-  individual: 100,
-  group: 90,
+  individual: INDIVIDUAL_TICKET_PRICE,
+  group: GROUP_TICKET_PRICE,
 };
--- app/metadata.tsx
+++ app/metadata.tsx
@@ -2,3 +2,5 @@ export const YEAR = 25; // Change to current year
 export const PHONE = "+20 1XX XXX XXXX"; // Change to Wallet Enabled phone number
 export const TELDA = "@alymob"; // Change to Telda username
 export const IPN = "XXXXXX"; // Change to Instapay details
+export const INDIVIDUAL_TICKET_PRICE = 100; // Change to individual ticket price
+export const GROUP_TICKET_PRICE = 90; // Change to group ticket price per person
