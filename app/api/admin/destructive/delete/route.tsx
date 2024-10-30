8ae4253242870f04beff36cf58c405d3aa6699d3
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-10-30 19:11:03 +0300

    exception to help development delete all prev rows

--- app/api/admin/destructive/delete/route.tsx
+++ app/api/admin/destructive/delete/route.tsx
@@ -13,7 +13,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
-  if (new Date() <= EVENT_DATE) {
+  if (new Date() <= EVENT_DATE && process.env.ADMIN_KEY !== "dev") {
     return NextResponse.json(
       { message: "Event has not concluded yet." },
       { status: 400 }
@@ -24,6 +24,8 @@ export async function GET(req: NextRequest) {
     await sql`DELETE FROM attendees`;
     await sql`DELETE FROM pay_backup`;
     await sql`DELETE FROM groups`;
+    await sql`ALTER SEQUENCE attendees_id_seq RESTART WITH 1`;
+    await sql`ALTER SEQUENCE groups_id_seq RESTART WITH 1`;
     return NextResponse.json({ message: "Data deleted." }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Error occurred." }, { status: 400 });
