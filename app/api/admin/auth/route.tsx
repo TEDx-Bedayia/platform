ef72e9711fec5e94079f6e8953692464f7190316
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-11-01 18:08:59 +0200

    Big Stupid Logical Security Error

--- app/api/admin/auth/route.tsx
+++ app/api/admin/auth/route.tsx
@@ -33,7 +33,7 @@ export async function POST(request: NextRequest) {
     if (
       username === process.env.ADMIN_USERNAME &&
       password === process.env.ADMIN_PASSWORD &&
-      params.get("name")?.toString() !== process.env.MAINTAINER &&
+      params.get("name")?.toString() === process.env.MAINTAINER &&
       process.env.ADMIN_KEY &&
       process.env.ADMIN_USERNAME &&
       process.env.ADMIN_PASSWORD
