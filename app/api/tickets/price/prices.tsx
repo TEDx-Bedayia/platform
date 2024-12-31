23337f6d3d22bfac4bcc3df5a575eaab6dd52126
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-12-31 20:45:50 +0200

    Remove 1% E-Wallet

--- app/api/tickets/price/prices.tsx
+++ app/api/tickets/price/prices.tsx
@@ -1,7 +1,6 @@
 import { GROUP_TICKET_PRICE, INDIVIDUAL_TICKET_PRICE } from "@/app/metadata";
 
 let markupFunction = (price: number, method: string) => {
-  if (method == "vfcash") return Math.round(price + price * 0.01);
   // return rounded price to nearest integer
   return Math.round(price);
 };
