630d7f4ae6872e481c970c374b79b93f2195d831
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2025-01-12 19:50:07 +0200

    Update Ticket Endpoint to Generate a UUID if attendee has paid but is missing a UUID... would be used for giveaway tickets.

--- app/api/admin/send-ticket/route.tsx
+++ app/api/admin/send-ticket/route.tsx
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { type NextRequest } from "next/server";
 import { sendEmail } from "../payment-reciever/eTicketEmail";
+import { safeRandUUID } from "../payment-reciever/main";
 
 export async function GET(request: NextRequest) {
   let params = request.nextUrl.searchParams;
@@ -21,6 +22,12 @@ export async function GET(request: NextRequest) {
     let q = await sql`SELECT * FROM attendees WHERE id = ${id}`;
     if (q.rowCount !== 1) {
       return Response.json({ message: "Attendee not found." }, { status: 404 });
+    }
+
+    if (q.rows[0].uuid === null && q.rows[0].paid == true) {
+      let uuid = await safeRandUUID();
+      await sql`UPDATE attendees SET uuid = ${uuid} WHERE id = ${id}`;
+      q.rows[0].uuid = uuid;
     }
 
     await sendEmail(q.rows[0].email, q.rows[0].full_name, q.rows[0].uuid);
