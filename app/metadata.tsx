e46d3570a8b1bcf56a9df910d3f7b5032470a351
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2025-06-17 16:19:46 +0300

    Edit Comments

--- app/metadata.tsx
+++ app/metadata.tsx
@@ -7,7 +7,7 @@ export const IPN = "omarelmandooh@instapay"; // Change to Instapay details
 export const INDIVIDUAL_TICKET_PRICE = 400; // Change to individual ticket price
 export const GROUP_TICKET_PRICE = 350; // Change to group ticket price per person
 
-export const DISCOUNTED_TICKET_PRICE = 300; // Change to discounted ticket price
+export const DISCOUNTED_TICKET_PRICE = 300; // Change to rush hour ticket price
 
 export const TICKET_WINDOW = [
   new Date("2025-01-01T22:00:00Z"),
@@ -17,13 +17,13 @@ export const EVENT_DATE = new Date("2025-01-31T23:59:59Z"); // Change to event d
 export const HOST = "https://tedxbedayia.com"; // Change to your domain
 
 export const EVENT_DESC =
-  "The event will be held on Friday, 31st of January, 2025 from 3 P.M. to 10 P.M. at Bedayia International School Gate 2.";
+  "The event will be held on Friday, 31st of January, 2025 from 3 P.M. to 10 P.M. at Bedayia International School. Entrance will be from Gate #2.";
 
 export const YEAR = 25;
 
 export const SPEAKER_FREE_TICKETS = 2; // Change to number of free tickets for speakers
 
 export const support: Author = {
-  name: "Aly Mobarak", // Change with your name (head)
-  url: "mailto:alymmobarak@hotmail.com", // Change with your public email
+  name: "Aly Mobarak",
+  url: "mailto:alymmobarak@hotmail.com",
 };
