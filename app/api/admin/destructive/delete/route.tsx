701f9b16ce99b0b679e35c0b69b29afc0b40d2dc
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2025-06-18 16:31:54 +0300

    Fix Bug due to schema change.

--- app/api/admin/destructive/delete/route.tsx
+++ app/api/admin/destructive/delete/route.tsx
@@ -39,13 +39,14 @@ export async function GET(req: NextRequest) {
   }
 
   try {
+    await sql`DELETE FROM groups`;
     await sql`DELETE FROM attendees`;
     await sql`DELETE FROM pay_backup`;
-    await sql`DELETE FROM groups`;
     await sql`ALTER SEQUENCE attendees_id_seq RESTART WITH 1`;
     await sql`ALTER SEQUENCE groups_grpid_seq RESTART WITH 1`;
     return NextResponse.json({ message: "Data deleted." }, { status: 200 });
   } catch (error) {
+    console.error("Error deleting data:", error);
     return NextResponse.json({ message: "Error occurred." }, { status: 400 });
   }
 }
