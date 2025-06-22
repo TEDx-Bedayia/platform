36f3e672a18463779d76fed57df8df43eff91c9f
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2025-06-22 17:46:30 +0300

    Refactor data deletion logic to ensure proper database connection handling and include additional cleanup for marketing members and rush hour data

--- app/api/admin/destructive/delete/route.tsx
+++ app/api/admin/destructive/delete/route.tsx
@@ -1,3 +1,4 @@
+import { SQLSettings } from "@/app/api/utils/sql-settings";
 import { EVENT_DATE } from "@/app/metadata";
 import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
@@ -39,11 +40,20 @@ export async function GET(req: NextRequest) {
   }
 
   try {
-    await sql`DELETE FROM groups`;
-    await sql`DELETE FROM attendees`;
-    await sql`DELETE FROM pay_backup`;
-    await sql`ALTER SEQUENCE attendees_id_seq RESTART WITH 1`;
-    await sql`ALTER SEQUENCE groups_grpid_seq RESTART WITH 1`;
+    const client = await sql.connect();
+    await client.sql`DELETE FROM groups`;
+    await client.sql`DELETE FROM attendees`;
+    await client.sql`DELETE FROM pay_backup`;
+    await client.sql`ALTER SEQUENCE attendees_id_seq RESTART WITH 1`;
+    await client.sql`ALTER SEQUENCE groups_grpid_seq RESTART WITH 1`;
+
+    await client.sql`DELETE FROM marketing_members`;
+    await client.sql`DELETE FROM rush_hour`;
+    await client.sql`ALTER SEQUENCE marketing_members_id_seq RESTART WITH 1`;
+    await client.sql`ALTER SEQUENCE rush_hour_id_seq RESTART WITH 1`;
+
+    await client.query(`SET ${SQLSettings.RUSH_HOUR_DATE} = NULL`);
+    client.release();
     return NextResponse.json({ message: "Data deleted." }, { status: 200 });
   } catch (error) {
     console.error("Error deleting data:", error);
