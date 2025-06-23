d6bc32efb2b726dbec9a894a81ba1e15e83020f9
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2025-06-23 21:33:18 +0300

    Add tutorials page to provide guidance on using the eTicket system

---
+++ app/tutorials/page.tsx
@@ -0,0 +1,11 @@
+export default function TutorialsPage() {
+  return (
+    <div>
+      <h1>Tutorials v1.1</h1>
+      <p>Welcome to the tutorials page!</p>
+      <p>
+        Here you can find various tutorials to help you use the eTicket system.
+      </p>
+    </div>
+  );
+}
