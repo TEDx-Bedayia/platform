8ebd6a639e11db9a591f9bfd7b76e0454821bb0f
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2025-09-16 23:30:38 +0300

    Update payment methods.

--- app/api/tickets/payment-methods/payment-methods.tsx
+++ app/api/tickets/payment-methods/payment-methods.tsx
@@ -16,43 +16,43 @@ export interface Field {
   id: string;
 }
 
-const TLDA: PaymentMethod = {
-  displayName: "Telda",
-  identifier: "TLDA",
-  to: TELDA,
-  fields: [
-    {
-      type: "string",
-      id: "tlda",
-      label: "Telda Username",
-      placeholder: "Telda Username",
-      required: true,
-    },
-  ],
-};
+// const TLDA: PaymentMethod = {
+//   displayName: "Telda",
+//   identifier: "TLDA",
+//   to: TELDA,
+//   fields: [
+//     {
+//       type: "string",
+//       id: "tlda",
+//       label: "Telda Username",
+//       placeholder: "Telda Username",
+//       required: true,
+//     },
+//   ],
+// };
 
-const INSTAPAY: PaymentMethod = {
-  displayName: "Instapay",
-  identifier: "IPN",
-  to: IPN,
-  fields: [
-    {
-      type: "string",
-      id: "ipn",
-      label: "InstaPay Address (IPA)",
-      placeholder: "InstaPay Address (without @instapay)",
-      required: true,
-    },
-  ],
-};
+// const INSTAPAY: PaymentMethod = {
+//   displayName: "Instapay",
+//   identifier: "IPN",
+//   to: IPN,
+//   fields: [
+//     {
+//       type: "string",
+//       id: "ipn",
+//       label: "InstaPay Address (IPA)",
+//       placeholder: "InstaPay Address (without @instapay)",
+//       required: true,
+//     },
+//   ],
+// };
 
-const VFCASH: PaymentMethod = {
-  displayName: "E-Wallet (e.g. Vodafone Cash)",
-  identifier: "VFCASH",
-  to: PHONE,
-  fields: [],
-  automatic: true,
-};
+// const VFCASH: PaymentMethod = {
+//   displayName: "E-Wallet (e.g. Vodafone Cash)",
+//   identifier: "VFCASH",
+//   to: PHONE,
+//   fields: [],
+//   automatic: true,
+// };
 
 const CASH: PaymentMethod = {
   displayName: "Bedayia High School Office (Cash)",
@@ -61,16 +61,16 @@ const CASH: PaymentMethod = {
   fields: [],
 };
 
-const CARD: PaymentMethod = {
-  displayName: "Credit/Debit Card (or Telda Card)",
-  identifier: "CARD",
-  to: "Paymob",
-  automatic: true,
-  fields: [],
-};
+// const CARD: PaymentMethod = {
+//   displayName: "Credit/Debit Card (or Telda Card)",
+//   identifier: "CARD",
+//   to: "Paymob",
+//   automatic: true,
+//   fields: [],
+// };
 
 export function getPaymentMethods() {
-  return [CARD, VFCASH, TLDA, INSTAPAY, CASH];
+  return [CASH];
 }
 
 export function getIdentifiersForPaymentMethods() {
