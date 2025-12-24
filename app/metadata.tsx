39dffe324d6613308358cd0bd0336aae4818001d
Aly Salah <147055589+AlyMobarak@users.noreply.github.com>
2025-12-24 16:52:49 +0200

    Update ticket prices, event date, and payment processor
    Early Bird, Event Date & Desc, Ticket Window

--- app/metadata.tsx
+++ app/metadata.tsx
@@ -5,16 +5,16 @@ export const VFCASH = "01003993559"; // Change to Vodafone Cash number
 export const TELDA = "@alykotb"; // Change to Telda username
 export const IPN = "reemabdulghaffar@instapay"; // Change to Instapay details
 
-export const INDIVIDUAL_TICKET_PRICE = 400; // Change to individual ticket price
-export const GROUP_TICKET_PRICE = 350; // Change to group ticket price per person
+export const INDIVIDUAL_TICKET_PRICE = 350; // Change to individual ticket price
+export const GROUP_TICKET_PRICE = 300; // Change to group ticket price per person
 
 export const DISCOUNTED_TICKET_PRICE = 300; // Change to rush hour ticket price
 
 export const TICKET_WINDOW = [
-  new Date("2025-08-01T22:00:00Z"),
-  new Date("2025-11-29T23:59:59Z"),
+  new Date("2026-01-01T22:00:00Z"),
+  new Date("2026-02-12T23:59:59Z"),
 ]; // Change to actual ticket window
-export const EVENT_DATE = new Date("2020-01-01T23:59:59Z"); // Change to event date
+export const EVENT_DATE = new Date("2026-02-12T23:59:59Z"); // Change to event date
 export const HOST = "https://tedxbedayia.com";
 
 export const EVENT_DESC = "TESTING PHASE. ANY QR CODE TICKETS ARE INVALID."; // Change to event description
@@ -28,4 +28,4 @@ export const support: Author = {
   url: "mailto:alymmobarak@hotmail.com",
 };
 
-export const paymentProcessor = "PayMob"; // Change to payment processor name
+export const paymentProcessor = "Telda, Instapay, and eWallet"; // Change to payment processor name
