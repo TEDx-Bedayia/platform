58bd1c8465be4d6c577294f4d7a063f68d5cdaa1
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2025-12-25 18:08:56 +0200

    Move prices file to correct location

---
+++ app/api/tickets/prices.tsx
@@ -0,0 +1,37 @@
+import {
+  DISCOUNTED_TICKET_PRICE,
+  GROUP_EARLY_PRICE,
+  GROUP_TICKET_PRICE,
+  INDIVIDUAL_EARLY_PRICE,
+  INDIVIDUAL_TICKET_PRICE,
+} from "@/app/metadata";
+import { TicketType } from "../../ticket-types";
+
+// Use this function to calculate the price of a ticket.
+const getPrice = (type: TicketType, method: string) => {
+  // return rounded price to nearest integer
+  let basePrice = INDIVIDUAL_TICKET_PRICE;
+  if (type === TicketType.GROUP) {
+    basePrice = GROUP_TICKET_PRICE;
+  } else if (type === TicketType.DISCOUNTED) {
+    basePrice = DISCOUNTED_TICKET_PRICE;
+  } else if (type === TicketType.TEACHER) {
+    basePrice = 0.5 * INDIVIDUAL_TICKET_PRICE;
+  } else if (type === TicketType.EARLY_BIRD_INDIVIDUAL) {
+    basePrice = INDIVIDUAL_EARLY_PRICE;
+  } else if (type === TicketType.EARLY_BIRD_GROUP) {
+    basePrice = GROUP_EARLY_PRICE;
+  } else if (type == TicketType.SPEAKER || type == TicketType.GIVEAWAY) {
+    return 0;
+  }
+
+  return Math.round(basePrice);
+};
+
+export let price = {
+  individual: INDIVIDUAL_TICKET_PRICE,
+  group: GROUP_TICKET_PRICE,
+  discounted: DISCOUNTED_TICKET_PRICE,
+  teacher: 0.5 * INDIVIDUAL_TICKET_PRICE,
+  getPrice: getPrice,
+};
